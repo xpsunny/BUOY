2a28dd44422656606d9de294c3f4f8ba97b714b9
xpsunny <sz@u.northwestern.edu>
2014-11-07 18:28:27 -0600

    wow

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Sunny\Documents\buoy - Copy\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Sunny\Documents\buoy - Copy\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Sunny\Documents\buoy - Copy\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Sunny\Documents\buoy - Copy\platforms\android\AndroidManifest.xml \
