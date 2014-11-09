fd75303a8fa8d7e3747e115e4a4dd5e9970b7ed9
xpsunny <sz@u.northwestern.edu>
2014-11-08 18:48:36 -0600

    iconography

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\Sunny\Documents\GitHub\BUOY\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Sunny\Documents\GitHub\BUOY\platforms\android\CordovaLib\AndroidManifest.xml \
+C:\Users\Sunny\Documents\GitHub\BUOY\BUOY\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Sunny\Documents\GitHub\BUOY\BUOY\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,12 +6,20 @@ module.exports = [
         "clobbers": [
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.inappbrowser": "0.5.3"
+    "org.apache.cordova.inappbrowser": "0.5.3",
+    "org.apache.cordova.splashscreen": "0.3.4"
 }
 // BOTTOM OF METADATA
 });
